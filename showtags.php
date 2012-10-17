fff9dbe3d18c81d9728f2734d2bc58d005cd24dc
Roberto Segura <roberto@phproberto.com>
2012-10-17 11:16:15 +0200

    [imp] Joomla! 3.0 compatible & 100% Joomla! coding standard

--- showtags.php
+++ showtags.php
@@ -1,218 +1,272 @@
 <?php
-
 /**
- * Content Showtags
- * Author: Roberto Segura - roberto@phproberto.com - www.phproberto.com
- * Copyright (c) 2012 Roberto Segura. All Rights Reserved.
- * License: GNU/GPL 2, http://www.gnu.org/licenses/gpl-2.0.html
+ * @package     Joomla.Plugin
+ * @subpackage  Content.Showtags
  *
- * Showtags for Joomla 2.5 by Roberto Segura
- *
+ * @author      Roberto Segura <roberto@phproberto.com>
+ * @copyright   (c) 2012 Roberto Segura. All Rights Reserved.
+ * @license     GNU/GPL 2, http://www.gnu.org/licenses/gpl-2.0.htm
  */
 
-//no direct access
 defined('_JEXEC') or die;
 
-jimport('joomla.plugin.plugin');
+JLoader::import('joomla.plugin.plugin');
 
 /**
- * Showtags content plugin
- * @author Roberto Segura - phproberto.com
+ * Main plugin class
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  Content.Showtags
+ * @since       2.5
  *
  */
-class plgContentShowtags extends JPlugin {
-    
-    const PLUGIN_NAME = 'showtags';
-    
-    private $_params = null;
-    
-    // paths
-    private $_pathPlugin = null;
-    private $_pathBoilerplates = null;
-    private $_pathCurrentBoilerplate = null;
-    
-    // urls
-    private $_urlPlugin = null;
-    private $_urlPluginJs = null;
-    private $_urlPluginCss = null;
-    
-    // url parameters
-    private $_option = null;
-    private $_view = null;
-    private $_id = null;
-    
-    // array of tags
-    private $_tags = array();
-    
-    function __construct( &$subject ){
-        
-        parent::__construct( $subject );
-    
-        // Load plugin parameters
-        $this->_plugin = JPluginHelper::getPlugin( 'content', 'showtags' );
-        $this->_params = new JRegistry( $this->_plugin->params );
-        
-        // init folder structure
-        $this->_initFolders();
-        
-        // load plugin language
-        $this->loadLanguage ('plg_'.$this->_plugin->type.'_'.$this->_plugin->name, JPATH_ADMINISTRATOR);
-    }
-    
-    
-    /**
+class PlgContentShowtags extends JPlugin
+{
+
+	const PLUGIN_NAME = 'showtags';
+
+	private $_params = null;
+
+	// Paths
+	private $_pathPlugin             = null;
+
+	private $_pathBoilerplates       = null;
+
+	private $_pathCurrentBoilerplate = null;
+
+	// URLs
+	private $_urlPlugin    = null;
+
+	private $_urlPluginJs  = null;
+
+	private $_urlPluginCss = null;
+
+	// URL parameters
+	private $_option = null;
+
+	private $_view   = null;
+
+	private $_id     = null;
+
+	// Array of tags
+	private $_tags = array();
+
+	/**
+	* Constructor
+	*
+	* @param   mixed  &$subject  Subject
+	*/
+	function __construct( &$subject )
+	{
+
+		parent::__construct($subject);
+
+		// Load plugin parameters
+		$this->_plugin = JPluginHelper::getPlugin('content', 'showtags');
+		$this->_params = new JRegistry($this->_plugin->params);
+
+		// Init folder structure
+		$this->_initFolders();
+
+		// Load plugin language
+		$this->loadLanguage('plg_' . $this->_plugin->type . '_' . $this->_plugin->name, JPATH_ADMINISTRATOR);
+	}
+
+	/**
      * Parse the tags before displaying content
-     * @param string $context
-     * @param object $article
-     * @param object $params
-     * @param integer $limitstart
+     *
+     * @param   string   $context     application context
+     * @param   object   &$article    the article object
+     * @param   object   &$params     parameters
+     * @param   integer  $limitstart  limit
+     *
+     * @return void
      */
-    public function onContentBeforeDisplay($context, &$article, &$params, $limitstart = 0 ) {
-        
-        // required objects
-        $app = JFactory::getApplication();
-        $document = JFactory::getDocument();
-        $jinput = $app->input;
-        
-        // get url parameters
-        $this->_option = $jinput->get('option',null);
-        $this->_view = $jinput->get('view',null);
-        $this->_id = $jinput->get('id',null);
+	public function onContentBeforeDisplay($context, &$article, &$params, $limitstart = 0 )
+	{
+		// Required objects
+		$app      = JFactory::getApplication();
+		$document = JFactory::getDocument();
+		$jinput   = $app->input;
 
-        $this->_article = $article;
+		// Get url parameters
+		$this->_option = $jinput->get('option', null);
+		$this->_view   = $jinput->get('view', null);
+		$this->_id     = $jinput->get('id', null);
 
-        // validate view
-        if ($context != 'com_content.article' || !$this->_validateView() || !isset($article->metakey) || empty($article->metakey)) {
-            return;
-        }
-      
-        $this->_tags = explode(',',$article->metakey);
-        $parsedTags = $this->_parseTags();
-        
-        $position = $this->_params->get('position', 'before');
-        $field = ($this->_view == 'category') ? 'introtext' : 'text';
+		$this->_article = $article;
 
-        switch ($position) {
-            case 'before':
-                $article->{$field} = $parsedTags . $article->{$field};
-                break;
-            case 'after':
-                $article->{$field} .= $parsedTags;
-                break;
-            // create a new article property called showtags with the parsed tags
-            case 'property':
-                $article->showtags = $parsedTags;
-                break;
-            default:
-                $article->{$field} = $parsedTags . $article->{$field} . $parsedTags;
-            break;
-        }
-        $document->addStyleSheet($this->_urlPluginCss.'/showtags.css');
-    }
-    
-    private function _initFolders() {
-        
-        // paths
-        $this->_pathPlugin = JPATH_PLUGINS . DIRECTORY_SEPARATOR . "content" . DIRECTORY_SEPARATOR . self::PLUGIN_NAME;
-        
-        // urls
-        $this->_urlPlugin = JURI::root()."plugins/content/showtags";
-        $this->_urlPluginJs = $this->_urlPlugin . "/js";
-        $this->_urlPluginCss = $this->_urlPlugin . "/css";
-    }
-	
-	private function _validateView() {
-	    
-	    if ($this->_option == 'com_content') {
+		// Validate view
+		if ($context != 'com_content.article' || !$this->_validateView()
+			|| !isset($article->metakey) || empty($article->metakey))
+		{
+			return;
+		}
 
-            // get active categories
-            $activeCategories = $this->_params->get('active_categories','');
-            if (!is_array($activeCategories)) {
-                $activeCategories = array($activeCategories);
-            }
+		$this->_tags = explode(',', $article->metakey);
+		$parsedTags  = $this->_parseTags();
+		$position    = $this->_params->get('position', 'before');
+		$field       = ($this->_view == 'category') ? 'introtext' : 'text';
 
-            // article view enabled?
-            if ($this->_view == 'article' && $this->_id && $this->_params->get('enable_article',0)) {
+		switch ($position)
+		{
+			case 'before':
+				$article->{$field} = $parsedTags . $article->{$field};
+				break;
 
-                // category filter
-                if ($activeCategories && $this->_article
-                    && ( in_array('-1', $activeCategories) || in_array($this->_article->catid, $activeCategories) )) {
-                    return true;
-                }
-            }
+			case 'after':
+				$article->{$field} .= $parsedTags;
+				break;
 
-            // category view enabled?
-            if ($this->_view == 'category' && $this->_id && $this->_params->get('enable_category',0)) {
+			// Create a new article property called showtags with the parsed tags
+			case 'property':
+				$article->showtags = $parsedTags;
+				break;
 
-                // category filter
-                if ($activeCategories
-                    && ( in_array('-1', $activeCategories) || in_array($this->_id, $activeCategories) )) {
-                    return true;
-                }
-
-            }
-	    }
-	    return false;
+			default:
+				$article->{$field} = $parsedTags . $article->{$field} . $parsedTags;
+			break;
+		}
+		$document->addStyleSheet($this->_urlPluginCss . '/showtags.css');
 	}
-	
-	private function _parseTags() {
 
-        // default value
-	    $html = '';
+	/**
+	 * Initialise folder structure
+	 *
+	 * @return void
+	 */
+	private function _initFolders()
+	{
 
-        // parse parameters
-	    $parentContainer = $this->_params->get('container','div');
-	    $customCss = $this->_params->get('css_class',null);
-	    $parseMode = $this->_params->get('format','ulli');
-        $wordlistSeparator = $this->_params->get('wordlist_separator',',');
-        $menulink= $this->_params->get('menulink',null);
-        $taxonomyActive = $this->_params->get('taxonomy_enabled',0);
-	    
-	    if ($this->_tags) {
-	        $html .= "\n<".$parentContainer." class=\"content-showtags ".$customCss."\">";
-	        if ($parseMode == 'ulli') {
-	            $html .= "\n\t<ul>";
-	        }
-	        $html .= '<span>'.JText::_('PLG_CONTENT_SHOWTAGS_TITLE').' </span>';
-	        $i = 0;
-	        foreach ($this->_tags as $tag) {
-                
-                // clear tag empty spaces
-	            $tag = trim($tag);
-	            
-                // generate the url
-                if ($taxonomyActive) {
-                    $url = 'index.php?option=com_taxonomy&tag=' . $tag;
-                } else {
-	                $url = 'index.php?option=com_search&searchword='. $tag . '&ordering=&searchphrase=all';
-                }
-                
-                // force Itemid?
-                if ($menulink) {
-                    $url .= "&Itemid=".$menulink;
-                }
-                
-                // build the route
-                $url = JRoute::_(JFilterOutput::ampReplace($url));
-                
-	            $tag = '<a href="'.$url.'" >'.$tag.'</a>';
-	            if ($parseMode == 'ulli') {
-	                $html .= "\n\t\t<li>".$tag."</li>";
-	            } else {
-	                if ($i) {
-	                    $html .= $wordlistSeparator . ' ';
-	                }
-	                $html .= $tag;
-	            }
-	            $i++;
-	        }
-	        if ($parseMode) {
-	            $html .= "\n\t</ul>";
-	        }
-	        $html .= "\n</".$parentContainer.">\n";
-	    }
-	    return $html;
+		// Paths
+		$this->_pathPlugin = JPATH_PLUGINS . DIRECTORY_SEPARATOR . "content" . DIRECTORY_SEPARATOR . self::PLUGIN_NAME;
+
+		// URLs
+		$this->_urlPlugin    = JURI::root() . "plugins/content/showtags";
+		$this->_urlPluginJs  = $this->_urlPlugin . "/js";
+		$this->_urlPluginCss = $this->_urlPlugin . "/css";
 	}
-	
+
+	/**
+	 * validate view url
+	 *
+	 * @return boolean
+	 *
+	 * @author Roberto Segura
+	 * @version 14/08/2012
+	 *
+	 */
+	private function _validateView()
+	{
+
+		if ($this->_option == 'com_content')
+		{
+			// Get active categories
+			$activeCategories = $this->_params->get('active_categories', '');
+			if (!is_array($activeCategories))
+			{
+				$activeCategories = array($activeCategories);
+			}
+
+			// Article view enabled?
+			if ($this->_view == 'article' && $this->_id && $this->_params->get('enable_article', 0))
+			{
+				// Category filter
+				if ($activeCategories && $this->_article
+					&& ( in_array('-1', $activeCategories) || in_array($this->_article->catid, $activeCategories) ))
+				{
+					return true;
+				}
+			}
+
+			// Category view enabled?
+			if ($this->_view == 'category' && $this->_id && $this->_params->get('enable_category', 0))
+			{
+				// Category filter
+				if ($activeCategories
+					&& ( in_array('-1', $activeCategories) || in_array($this->_id, $activeCategories) ))
+				{
+					return true;
+				}
+
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Parse the tags into HTML
+	 *
+	 * @return   string  The HTML code with the parsed tags
+	 */
+	private function _parseTags()
+	{
+		// Default value
+		$html = '';
+
+		// Parse parameters
+		$parentContainer   = $this->_params->get('container', 'div');
+		$customCss         = $this->_params->get('css_class', null);
+		$parseMode         = $this->_params->get('format', 'ulli');
+		$wordlistSeparator = $this->_params->get('wordlist_separator', ',');
+		$menulink          = $this->_params->get('menulink', null);
+		$taxonomyActive    = $this->_params->get('taxonomy_enabled', 0);
+
+		if ($this->_tags)
+		{
+			$html .= "\n<" . $parentContainer . " class=\"content-showtags " . $customCss . "\">";
+			if ($parseMode == 'ulli')
+			{
+				$html .= "\n\t<ul>";
+			}
+			$html .= '<span>' . JText::_('PLG_CONTENT_SHOWTAGS_TITLE') . ' </span>';
+			$i = 0;
+			foreach ($this->_tags as $tag)
+			{
+				// Clear tag empty spaces
+				$tag = trim($tag);
+
+				// Generate the url
+				if ($taxonomyActive)
+				{
+					$url = 'index.php?option=com_taxonomy&tag=' . $tag;
+				}
+				else
+				{
+					$url = 'index.php?option=com_search&searchword=' . $tag . '&ordering=&searchphrase=all';
+				}
+
+				// Force Itemid?
+				if ($menulink)
+				{
+					$url .= "&Itemid=" . $menulink;
+				}
+
+				// Build the route
+				$url = JRoute::_(JFilterOutput::ampReplace($url));
+
+				$tag = '<a href="' . $url . '" >' . $tag . '</a>';
+				if ($parseMode == 'ulli')
+				{
+					$html .= "\n\t\t<li>" . $tag . "</li>";
+				}
+				else
+				{
+					if ($i)
+					{
+						$html .= $wordlistSeparator . ' ';
+					}
+					$html .= $tag;
+				}
+				$i++;
+			}
+			if ($parseMode)
+			{
+				$html .= "\n\t</ul>";
+			}
+			$html .= "\n</" . $parentContainer . ">\n";
+		}
+		return $html;
+	}
+
 }
-?>
